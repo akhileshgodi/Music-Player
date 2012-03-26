54e6ef62d415a27b6160486aceaff2f25de24f1c
Akhilesh Godi <akhileshgodi93@gmail.com>
2012-03-26 23:58:25 +0530

    Added new player. Implemented Levenshtein Distance algorithm

---
+++ src/iitm/apl/player/LevenshteinsDistance.java
@@ -0,0 +1,62 @@
+package iitm.apl.player;
+
+/**
+ * @author : Akhilesh Godi (CS10B037)
+ * Implementing the Levenshtein's distance  
+ */
+public class LevenshteinsDistance 
+{
+	public int getDistance(Object object1, Object object2) 
+	{
+		
+		String string1 = (String) object1;
+		String string2 = (String) object2;
+		
+		int distance[][]; 	// Distance Matrix
+        int n, m; 			// Length of first , second strings respectively
+        int i, j;			//Iterators for the first and second string respectively.
+        
+        /*Consider two arrays S and T to be treated as first and second respectively.*/
+
+        n = string1.length();
+        m = string2.length();
+        if (n == 0)
+            return m;
+        if (m == 0)
+            return n;
+        distance = new int[n+1][m+1];
+
+        for (i = 0; i <= n; i++)
+            distance[i][0] = i;
+        for (j = 0; j <= m; j++)
+            distance[0][j] = j;
+
+        for (i = 1; i <= n; i++)
+        {
+            for (j = 1; j <= m; j++)
+            {
+                if (string1.charAt (i - 1) == string2.charAt(j - 1) )
+                    distance[i][j] = distance[i-1][j-1];
+                else
+                	distance[i][j] = findMinimum(distance[i-1][j]+1, distance[i][j-1]+1, distance[i-1][j-1] + 1);
+            }
+        }
+
+        return distance[n][m];
+	}
+	
+	private int findMinimum(int a, int b, int c) 
+	{
+        int min = a;
+        if (b < min) 
+        {
+            min = b;
+        }
+        if (c < min) 
+        {
+            min = c;
+        }
+        return min;
+    }
+
+}
